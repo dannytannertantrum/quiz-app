2f1213b3f135f4a96dd2870941d6c24834e2b023
Jaime Farnan <jaimefarnan@gmail.com>
2023-07-04 08:41:56 -0400

    Add Tortoise ORM and Aerich for migrations

--- project/app/main.py
+++ project/app/main.py
@@ -1,7 +1,22 @@
-from app.config import get_settings, Settings
+import os
+
 from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.config import get_settings, Settings
 
 app = FastAPI()
+
+
+# https://tortoise.github.io/contrib/fastapi.html
+# Tortoise automatically cleans the connection up behind the scenes
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,  # False so a migration tool (Aerich) can do this for us
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
