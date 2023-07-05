3aafc3edb50902cddc41704b647a5b2e8471bc04
Jaime Farnan <jaimefarnan@gmail.com>
2023-07-05 07:54:04 -0400

    Add docstring and comment for lifespan

--- project/app/main.py
+++ project/app/main.py
@@ -12,6 +12,11 @@ log = logging.getLogger("uvicorn")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    """
+    This "lifespan" function under the asynccontextmanager decorator is how FastAPI
+    defines code that should be executed before the app starts up and code that should
+    be executed when the app is shutting down
+    """
     log.info("Starting up...")
 
     app.include_router(ping.router)
@@ -23,4 +28,5 @@ async def lifespan(app: FastAPI):
     log.info("Shutting down...")
 
 
+# https://fastapi.tiangolo.com/advanced/events/ - see docstrings above
 app = FastAPI(lifespan=lifespan)
