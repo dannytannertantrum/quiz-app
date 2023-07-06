27ca283079f34cc3fe99e1205d337b07a2c5f7e8
Jaime Farnan <jaimefarnan@gmail.com>
2023-07-06 15:36:16 -0400

    Add Pytest (again) and first test

---
+++ project/tests/conftest.py
@@ -0,0 +1,25 @@
+import os
+
+import pytest
+from fastapi.testclient import TestClient
+
+from app import main
+from app.config import get_settings, Settings
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
+
+
+# Remdiner: scope="module" means the fixture will be invoked once per test module
+# It's created for the module, used for all tests, then destroyed after all tests
+@pytest.fixture(scope="module")
+def test_app():
+    # set up - if we drill into dependency_overrides, we can see it's a dict of key/value pairs
+    # The key is the dependency name and value is what we'd like to override it with
+    main.app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(main.app) as test_client:
+        # testing
+        yield test_client
+
+    # tear down
