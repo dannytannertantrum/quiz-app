ab1b247b4f685a14cf5546b481ecd452bde4cfd9
Jaime Farnan <jaimefarnan@gmail.com>
2023-07-06 09:39:03 -0400

    Revert last 4 commits to scrap Tortoise and use SqlAlchemy

--- project/app/main.py
+++ project/app/main.py
@@ -1,32 +1,13 @@
-from contextlib import asynccontextmanager
-import logging
+from app.config import get_settings, Settings
+from fastapi import Depends, FastAPI
 
-from fastapi import FastAPI
-
-from app.api import ping
-from app.database import init_db
+app = FastAPI()
 
 
-log = logging.getLogger("uvicorn")
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """
-    This "lifespan" function under the asynccontextmanager decorator is how FastAPI
-    defines code that should be executed before the app starts up and code that should
-    be executed when the app is shutting down
-    """
-    log.info("Starting up...")
-
-    app.include_router(ping.router)
-    init_db(app)
-
-    yield
-
-    # After yielding, we clean up and shut down
-    log.info("Shutting down...")
-
-
-# https://fastapi.tiangolo.com/advanced/events/ - see docstrings above
-app = FastAPI(lifespan=lifespan)
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
