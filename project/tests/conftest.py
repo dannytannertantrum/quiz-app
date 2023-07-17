e4e9b7890475fe8c295c1a8c8438a298ceb9f211
Jaime Farnan <jaimefarnan@gmail.com>
2023-07-17 16:42:00 -0400

    Clean up some comments in conftest

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -42,12 +42,8 @@ def get_settings_override():
     )
 
 
-# Remdiner: scope="module" means the fixture will be invoked once per test module
-# It's created for the module, used for all tests, then destroyed after all tests
 @pytest.fixture(scope="module")
 def client() -> Generator:
-    # set up - if we drill into dependency_overrides, we can see it's a dict of key/value pairs
-    # The key is the dependency name and value is what we'd like to override it with
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         yield test_client
