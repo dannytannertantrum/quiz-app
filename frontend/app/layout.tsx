64188dc5afa67ae4dc82ee33530fb71e34e6b036
Jaime Farnan <jaimefarnan@gmail.com>
2023-08-07 15:59:22 -0400

    Add first component - color scheme toggle - and use atomic design principle

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'Quiz App',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>{children}</body>
     </html>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,10 @@
+import ColorSchemeToggle from './components/atoms/ColorSchemeToggle';
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center gap-4 p-24'>
       <h2 className='text-6xl'>Hello, World!</h2>
+      <ColorSchemeToggle />
     </main>
   );
 }
