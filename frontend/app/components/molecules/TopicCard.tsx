08dbba1d88eaec152afc49236422c5ffd4d7bd37
Jaime Farnan <jaimefarnan@gmail.com>
2023-10-04 17:29:30 -0400

    Style tweaks to the TopicCard

--- frontend/app/components/molecules/TopicCard.tsx
+++ frontend/app/components/molecules/TopicCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 export interface TopicCardProps {
   description: string;
   title: string;
@@ -7,16 +9,16 @@ export interface TopicCardProps {
 const getAppliedClasses = (color: string): string => {
   switch (color) {
     case 'emerald':
-      return 'bg-emerald-100 border-emerald-950 text-emerald-950 dark:bg-emerald-950 dark:border-emerald-100 dark:text-emerald-100';
+      return 'bg-emerald-200 border-emerald-950 text-emerald-950 dark:bg-emerald-950 dark:border-emerald-200 dark:text-emerald-200';
     case 'yellow':
-      return 'bg-yellow-100 border-yellow-950 text-yellow-950 dark:bg-yellow-950 dark:border-yellow-100 dark:text-yellow-100';
+      return 'bg-yellow-200 border-yellow-950 text-yellow-950 dark:bg-yellow-950 dark:border-yellow-200 dark:text-yellow-200';
     default:
-      return 'bg-cyan-100 border-cyan-950 text-cyan-950 dark:bg-cyan-950 dark:border-cyan-100 dark:text-cyan-100';
+      return 'bg-cyan-200 border-cyan-950 text-cyan-950 dark:bg-cyan-950 dark:border-cyan-200 dark:text-cyan-200';
   }
 };
 
 export const TopicCard = ({ color = 'cyan', description, title }: TopicCardProps) => {
-  const baseClasses = 'rounded-lg p-4 border-2 ';
+  const baseClasses = 'rounded-lg p-4 border-2 md:h-full ';
   const colorVariations = getAppliedClasses(color);
   const appliedClasses = baseClasses + colorVariations;
 
