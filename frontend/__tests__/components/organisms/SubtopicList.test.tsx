862fc59099459260e0565317c76d5ce22102c010
Jaime Farnan <jaimefarnan@gmail.com>
2023-10-14 14:18:30 -0400

    Wire up validation and form behavior for SubtopicList organism

--- frontend/__tests__/components/organisms/SubtopicList.test.tsx
+++ frontend/__tests__/components/organisms/SubtopicList.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 
 import { subtopicsTestData } from '../../../test-utils/shared-data';
-import { render, screen } from 'custom-rtl';
+import { render, renderWithUserEvent, screen } from 'custom-rtl';
 import { SubtopicList } from '../../../app/components/organisms/SubtopicList';
 
 describe('SubtopicList', () => {
@@ -12,5 +12,14 @@ describe('SubtopicList', () => {
     expect(screen.getByText('drama')).toBeInTheDocument();
     expect(screen.getByText('horror')).toBeInTheDocument();
     expect(screen.getByText('sci-fi')).toBeInTheDocument();
+  });
+
+  test('error message displays when user tries to submit the form without info', async () => {
+    const { user } = renderWithUserEvent(<SubtopicList subtopics={subtopicsTestData} />);
+    const submitButton = screen.getByText('Submit');
+
+    await user.click(submitButton);
+
+    expect(screen.getByText('Please select at least one topic')).toBeInTheDocument();
   });
 });
--- frontend/app/components/organisms/SubtopicList.tsx
+++ frontend/app/components/organisms/SubtopicList.tsx
@@ -1,31 +1,43 @@
 'use client';
 
+import { useState } from 'react';
+
 import { BaseTopicData } from '../../types/topics';
 import { Button } from '../atoms/Button';
-import { Fieldset } from '../atoms/Fieldset';
 import { Checkbox } from '../molecules/Checkbox';
-
-/*
-- Move list into SubtopicList - DONE
-- Get all subtopics on the page in Checkbox form - DONE
-- Style the background the same as the authform - DONE
-- COMMIT before adding functionality - DONE
-- Wire up the form
-  - Add form element
-  - Add fieldset
-  - Add submit button
-  - log out data for now
-  - Add validation
-    - Cannot submit form without checking anything
-    - (add error message) Do not put error message there until form touched
-    - When clicking submit, add "disabled" to fieldset
-*/
+import { Fieldset } from '../atoms/Fieldset';
 
 export const SubtopicList = ({ subtopics }: { subtopics: BaseTopicData[] }) => {
+  const [error, setError] = useState(false);
+  const [formTouched, setFormTouched] = useState(false);
+  let checkboxNodes: NodeListOf<HTMLInputElement> | null = null;
+  let checkboxes: HTMLInputElement[] = [];
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    setFormTouched(true);
     event.preventDefault();
 
-    console.log('submitted!');
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const selectedSubtopicUuids = Object.values(Object.fromEntries(formData.entries()));
+
+    selectedSubtopicUuids.length < 1 ? setError(true) : setError(false);
+  };
+
+  const handleCheckboxChange = () => {
+    setFormTouched(true);
+
+    if (checkboxNodes === null) {
+      checkboxNodes = document.querySelectorAll('input[type="checkbox"]');
+      checkboxes = Array.from(checkboxNodes);
+    }
+    const someCheckboxesChecked = checkboxes.some((box: HTMLInputElement) => box.checked);
+
+    if (formTouched && someCheckboxesChecked) {
+      setError(false);
+    } else if (!someCheckboxesChecked) {
+      setError(true);
+    }
   };
 
   return (
@@ -35,18 +47,26 @@ export const SubtopicList = ({ subtopics }: { subtopics: BaseTopicData[] }) => {
       onSubmit={handleSubmit}
       className={`w-full bg-thunder-200 border-y border-thunder-800 p-7 shadow-lg shadow-thunder-500
     dark:bg-thunder-950 dark:border-thunder-300 dark:shadow-thunder-800
-    md:w-[500px] md:border md:rounded-xl`}
+    md:w-[500px] md:border md:rounded-xl ${error && 'border-rose-900 dark:border-rose-300'}`}
       noValidate
     >
       <Fieldset disabled={false}>
         <ul>
           {subtopics.map((topic) => (
             <li className='mb-4' key={topic.id}>
-              <Checkbox id={topic.title} name={topic.title} />
+              <Checkbox
+                id={topic.title}
+                name={topic.title}
+                handleChange={handleCheckboxChange}
+                value={topic.id}
+              />
             </li>
           ))}
         </ul>
         <Button type='submit'>Submit</Button>
+        {error && (
+          <p className='pt-5 text-rose-900 dark:text-rose-300'>Please select at least one topic</p>
+        )}
       </Fieldset>
     </form>
   );
