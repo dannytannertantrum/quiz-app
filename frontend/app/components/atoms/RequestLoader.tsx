52e5fcb903e35b9d4b0592430cefd1dd82830f72
Jaime Farnan <jaimefarnan@gmail.com>
2023-10-21 12:41:00 -0400

    Add optional animationEnd handler for RequestLoader atom

--- frontend/app/components/atoms/RequestLoader.tsx
+++ frontend/app/components/atoms/RequestLoader.tsx
@@ -1,8 +1,13 @@
 /**
  *
- * @returns a giant question mark that animates a fill
+ * @returns a giant question mark that animates a bg-color fill from bottom to top
+ *
  * Styles can be found in global.css
  */
-export const RequestLoader = () => {
-  return <h3 className='requestLoader'>?</h3>;
+export const RequestLoader = ({ handleAnimationEnd }: { handleAnimationEnd?: () => void }) => {
+  return (
+    <h3 className='requestLoader' onAnimationEnd={handleAnimationEnd}>
+      ?
+    </h3>
+  );
 };
