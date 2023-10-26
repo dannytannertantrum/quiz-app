9bb4785f336a83eb9d571a81a4898d415e007ee3
Jaime Farnan <jaimefarnan@gmail.com>
2023-10-26 13:22:23 -0400

    Update layouts for topics and quizzes

--- frontend/app/quizzes/[id]/layout.tsx
+++ frontend/app/quizzes/[id]/layout.tsx
@@ -1,3 +1,7 @@
 export default function QuizzesLayout({ children }: { children: React.ReactNode }) {
-  return <main className='flex flex-col items-center gap-4 p-7'>{children}</main>;
+  return (
+    <main className='flex flex-col items-center gap-4 p-7 mx-auto max-w-7xl md:py-7 md:px-0'>
+      {children}
+    </main>
+  );
 }
