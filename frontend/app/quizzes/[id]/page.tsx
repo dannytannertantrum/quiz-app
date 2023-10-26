883270f85656fdef290c01c4f03daa03a89b37ff
Jaime Farnan <jaimefarnan@gmail.com>
2023-10-25 20:36:11 -0400

    Add dynamic metadata and all quiz question data to quizzes page

--- frontend/app/quizzes/[id]/page.tsx
+++ frontend/app/quizzes/[id]/page.tsx
@@ -3,11 +3,22 @@ import type { Metadata } from 'next';
 
 import { BASE_SERVER_URL, RESPONSE_ERROR } from '../../utils/constants';
 import { BaseQuizData } from '../../types/quizzes';
+import { QuizQuestionsAllData } from '../../types/quizQuestions';
 
-export const metadata: Metadata = {
-  title: 'Your Quiz | Quiz App',
-  description: 'You can do it!',
-};
+// Dynamic metadata
+// https://nextjs.org/docs/app/building-your-application/optimizing/metadata#dynamic-metadata
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const quiz: BaseQuizData = await fetch(`${BASE_SERVER_URL}/quizzes/${params.id}`, {
+    headers: { Cookie: cookies().toString() },
+  }).then((res) => res.json());
+
+  return {
+    title: `${
+      quiz?.primary_topic.charAt(0).toUpperCase() + quiz?.primary_topic.slice(1)
+    } | Quiz App`,
+    description: 'You can do it!',
+  };
+}
 
 export async function getQuiz(id: string) {
   const response = await fetch(`${BASE_SERVER_URL}/quizzes/${id}`, {
@@ -22,15 +33,29 @@ export async function getQuiz(id: string) {
   return quiz;
 }
 
+export async function getQuizQuestions(id: string) {
+  const response = await fetch(`${BASE_SERVER_URL}/quiz-questions/quiz/${id}`, {
+    headers: { Cookie: cookies().toString() },
+  });
+  if (response.status === 404) return null;
+  if (!response.ok) {
+    throw new Error(RESPONSE_ERROR);
+  }
+  const quizQuestions: QuizQuestionsAllData = await response.json();
+
+  return quizQuestions;
+}
+
 export default async function Quizzes({ params }: { params: { id: string } }) {
   let quiz: BaseQuizData | null = null;
+  let quizQuestions: QuizQuestionsAllData | null = null;
   try {
-    quiz = await getQuiz(params.id);
+    [quiz, quizQuestions] = await Promise.all([getQuiz(params.id), getQuizQuestions(params.id)]);
   } catch (reason: unknown) {
     console.error('There was a problem getting the quiz: ', reason);
   }
 
-  if (quiz === null) return <h2 className='text-3xl'>No quiz found</h2>;
+  if (quiz === null || quizQuestions === null) return <h2 className='text-3xl'>No quiz found</h2>;
 
   return (
     <>
--- frontend/app/types/quizQuestions.ts
+++ frontend/app/types/quizQuestions.ts
@@ -1,7 +1,15 @@
+import { AnswerOptions } from './questions';
+
 export interface QuizQuestions {
   // all strings are UUIDs
   id: string;
   question_id: string;
   quiz_id: string;
   user_answer: number;
+}
+
+export interface QuizQuestionsAllData extends QuizQuestions {
+  question: string;
+  answer_options: AnswerOptions[];
+  question_type: 'multiple choice';
 }
